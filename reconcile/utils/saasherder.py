a208fc6efdffde69139fb304b705b43467455be7
Rob Rati <rrati@redhat.com>
2021-02-17 14:54:48 -0500

    Use native openshift client for get requests

--- reconcile/utils/saasherder.py
+++ reconcile/utils/saasherder.py
@@ -476,7 +476,7 @@ class SaasHerder():
                         + f"{image_uri}: {str(e)}")
                     return None, None, None
 
-            oc = OC('server', 'token', local=True)
+            oc = OC(server=None, token=None, local=True)
             try:
                 resources = oc.process(template, consolidated_parameters)
             except StatusCodeError as e:
